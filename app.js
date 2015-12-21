b1c859370a840890c068c80af128d84c42dd20af
Xavier JUERY <xavier@juery.fr>
2015-12-21 23:07:04 +0100

    Backup

--- app.js
+++ app.js
@@ -3,55 +3,70 @@ var express = require('express');
 
 var configFile = './config.json';
 process.argv.forEach(function (val, index, array) {
-    if(index === 2){
+    if (index === 2) {
         console.log("Default config file overridden by :");
         console.log(val);
         configFile = val;
     }
 });
 
-//var config = require(configFile);
 var config = JSON.parse(require('fs').readFileSync(configFile, 'utf8'));
 
 // Create app
 var app = module.exports = express();
 
 //Check the number of configured endpoints
-console.log("Found "+config.length+" endpoints");
+console.log("Found " + config.length + " endpoints");
 
 //Load the endpoints
 function addEndpoint(element, index, array) {
-    //Verify which type of response must be sent
-    var response = "";
-    if(element.responseFile){
-        response = JSON.parse(require('fs').readFileSync(element.responseFile, 'utf8'));
-    }else{
-        response = element.response;
-    }
-
     //Declare each endpoint
-    if(element.httpMethod === "POST"){
-        app.post(element.url, function(req, res) {
-            res.send(response);
+    if (element.httpMethod === "POST") {
+        app.post(element.url, function (req, res) {
+            res.send(getResponseForEndpoint(configFile, element.url));
         });
-    }else if (element.httpMethod === "PUT"){
-        app.put(element.url, function(req, res) {
-            res.send(response);
+    } else if (element.httpMethod === "PUT") {
+        app.put(element.url, function (req, res) {
+            res.send(getResponseForEndpoint(configFile, element.url));
         });
-    }else if (element.httpMethod === "DELETE"){
-        app.delete(element.url, function(req, res) {
-            res.send(response);
+    } else if (element.httpMethod === "DELETE") {
+        app.delete(element.url, function (req, res) {
+            res.send(getResponseForEndpoint(configFile, element.url));
         });
-    }else{//GET
-        app.get(element.url, function(req, res) {
+    } else {//GET
+        app.get(element.url, function (req, res) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.send(response);
+            res.send(getResponseForEndpoint(configFile, element.url));
         });
     }
 }
 
+function getResponseForEndpoint(cfgfile, url) {
+    var cfg = JSON.parse(require('fs').readFileSync(cfgfile, 'utf8'));
+
+    var resp = null;
+    cfg.forEach(function (element, index, array) {
+        if (element.url == url) {
+            if (element.responseFile) {
+                resp = JSON.parse(require('fs').readFileSync(element.responseFile, 'utf8'));
+            } else {
+                resp = element.response;
+            }
+        }
+    });
+    return resp;
+}
+
 config.forEach(addEndpoint);
 
+// Add the special endpoint for populating specific responses in the endpoints
+
+
 // Default port is 3000
-app.listen(3000);
-console.log("Open Source Mockup Center is ready");
+var port = 3000;
+app.listen(port);
+console.log("MockCenter is ready and listening on port "+port);
+var os = require("os");
+config.forEach(function(element, index, array){
+    console.log("-> http://"+os.hostname().toLowerCase()+":"+port+element.url);
+});
